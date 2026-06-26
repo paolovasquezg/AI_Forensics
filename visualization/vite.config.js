f1f09d474e4304ca1704d080e43ede6a18494135
Paolo Vásquez <83627663+paolovasquezg@users.noreply.github.com>
2026-06-26 01:01:50 -0500

    allow tunellign

--- visualization/vite.config.js
+++ visualization/vite.config.js
@@ -8,6 +8,9 @@ import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  server: {
+    allowedHosts: ['t-shirt-silver-unshaven.ngrok-free.dev']
+  },
   plugins: [
     tailwindcss(),
     react(),
